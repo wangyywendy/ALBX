91be9870a8f13cebe06fb17d9c191dad6b52cc93
wangyywendy <wangyy.wendy@gmail.com>
2019-03-29 20:26:11 +0800

    删除目录

--- admin/category-delete.php
+++ admin/category-delete.php
@@ -6,17 +6,10 @@
 
 require_once '../functions.php';
 
-if (empty($_GET['id'])) {
+if (!empty($_GET['id'])) {
   //exit('缺少必要参数');
   xiu_execute(sprintf('delete from categories where id in (%s)', $_GET['id']));
 }
-
-//$id = $_GET['id'];
-
-//$rows = xiu_execute('delete from categories where id in (' . $id . ');');
-
-// if ($rows > 0) {}
-//header('Location: /admin/categories.php');
 
 $target = empty($_SERVER['HTTP_REFERER']) ? '/admin/categories.php' : $_SERVER['HTTP_REFERER'];
 header('Location: ' . $target);
